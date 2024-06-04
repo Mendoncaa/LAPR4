9fd776c663c4523d43a1f4dc59e19adc17b14ee7
ManuelPimentel_1210670 <1210670@isep.ipp.pt>
2024-06-04 19:07:24 +0100

    #39 Fixed Bug with ranks not allowing the project to build correctly

--- jobs4u.core/src/main/java/core/management/RecruitmentProcess/PhaseValidatorService.java
+++ jobs4u.core/src/main/java/core/management/RecruitmentProcess/PhaseValidatorService.java
@@ -4,6 +4,7 @@ import core.infrastructure.persistence.PersistenceContext;
 import core.management.RecruitmentProcess.domain.RecruitmentProcess;
 import core.management.jobApplication.repository.ApplicationRepository;
 import core.management.jobOpening.domain.JobReference;
+import core.management.jobOpening.repository.JobOpeningRepository;
 import core.management.rank.domain.Rank;
 import core.management.rank.repository.RankRepository;
 import eapli.framework.domain.services.DomainService;
@@ -18,15 +19,17 @@ public class PhaseValidatorService {
     private RecruitmentProcess recruitmentProcess;
     private ApplicationRepository applicationRepository;
     private RankRepository rankRepository;
+    private JobOpeningRepository jobOpeningRepository;
 
     public PhaseValidatorService(RecruitmentProcess recruitmentProcess) {
         this.recruitmentProcess = recruitmentProcess;
         applicationRepository = PersistenceContext.repositories().application();
         rankRepository = PersistenceContext.repositories().rank();
+        jobOpeningRepository = PersistenceContext.repositories().jobOpenings();
     }
 
     public Iterable<String> validatePhaseTransition(String jobReference) {
-        Rank rank = rankRepository.findByJobReference(new JobReference(jobReference));
+        Rank rank = jobOpeningRepository.findRankByJobReference(new JobReference(jobReference));
         int numberOfApplications = applicationRepository.countApplicationsByJobReference(jobReference);
         int numberOfApplicationsScreening = applicationRepository.countScreeningByJobReference(jobReference);
         int numberOfApplicationsInterviews = applicationRepository.countInterviewsByJobReference(jobReference);
